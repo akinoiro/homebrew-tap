5f9b34381d50bb8fa4dcfac94d673238f86a31b7
akinoiro <hatethmirrors@gmail.com>
2025-09-17 11:11:16 +0000

    Update licence

--- Formula/ssh-list.rb
+++ Formula/ssh-list.rb
@@ -1,11 +1,11 @@
 # ssh-list.rb
 
 class SshList < Formula
-    desc "SSH connection manager with a TUI interface "
+    desc "SSH connection manager with a TUI interface"
     homepage "https://github.com/akinoiro/ssh-list"
     url "https://github.com/akinoiro/ssh-list/archive/refs/tags/v1.5.0.tar.gz"
     sha256 "edc37f3fba6780e762027a82fa8508e2a9d01351e3402db81de263d47a22d8ae"
-    license "GPL-3.0"
+    license "GPL-3.0-or-later"
   
     depends_on "rust" => :build
   
