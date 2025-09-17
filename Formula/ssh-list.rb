1e54ae23efb34e61c660ed7c642b40dfa0206171
akinoiro <hatethmirrors@gmail.com>
2025-09-17 10:48:31 +0000

    change license

--- Formula/ssh-list.rb
+++ Formula/ssh-list.rb
@@ -5,6 +5,7 @@ class SshList < Formula
     homepage "https://github.com/akinoiro/ssh-list"
     url "https://github.com/akinoiro/ssh-list/archive/refs/tags/v1.5.0.tar.gz"
     sha256 "edc37f3fba6780e762027a82fa8508e2a9d01351e3402db81de263d47a22d8ae"
+    license "GPL-3.0"
   
     depends_on "rust" => :build
   
